053b85c360c1c0010ac5937222fcb1a62a0eb6eb
Will Lee <will.i.lee@outlook.com>
2018-06-11 20:45:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,9 +56,8 @@ def find_a(array)
 end
 
 def sum_array(array)
-  sum = 0
-  array.collect do |integer|
-    sum += integer
+  array.inject do |sum, integer|
+    sum + integer
   end
   return sum
 end
