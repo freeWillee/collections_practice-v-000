654d0677d26bfff8c99a40316204e2bec452dbcf
Will Lee <will.i.lee@outlook.com>
2018-06-11 20:32:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,3 +37,14 @@ def swap_elements_from_to(array, index, destination_index)
   array[destination_index], array[index] = array[index], array[destination_index]
   return array
 end
+
+def reverse_array(int_array)
+  int_array.sort do |a,b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+end
