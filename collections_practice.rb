1ca00a8b192126c13a18c6f88b43b1dfd8b62f12
Will Lee <will.i.lee@outlook.com>
2018-06-11 20:04:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,3 +2,15 @@
 def sort_array_asc(int_array)
   int_array.sort
 end
+
+def sort_array_desc(int_array)
+  int_array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+
+end
