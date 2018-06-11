10f1ebc435d096368d5792b402e9bd1ac4fa0efd
Will Lee <will.i.lee@outlook.com>
2018-06-11 20:25:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,8 +28,5 @@ def sort_array_char_count(string_array)
 end
 
 def swap_elements(array)
-  swap_array = []
-  swap_array << array[2]
-  swap_array << array[3]
-
+  array[1], array[2] = array[2], array[1]
 end
